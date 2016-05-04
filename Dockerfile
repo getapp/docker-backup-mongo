33a1f7affcd117216f0a55a318a62f4b731a588c
Boris Mikhaylov <kaguxmail@gmail.com>
2016-05-04 17:49:51 +0200

    updated model and removed jobs

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,12 @@ FROM quay.io/getapp/docker-backup
 
 MAINTAINER Boris Mikhaylov
 
+# add mongodb repository
+RUN \
+    apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10 && \
+    echo "deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen" | tee /etc/apt/sources.list.d/mongodb.list
+
 RUN apt-get update \
-    && apt-get install -y mysql-client \
+    && apt-get install -y mongodb-org-tools \
     && apt-get autoremove -y -qq \
     && apt-get clean -qq
---
+++ models/mongo.rb
@@ -0,0 +1,21 @@
+Model.new(:mongo, 'Mongo Backup') do
+  database MongoDB do |db|
+    db.name               = ENV['MONGO_DATABASE']
+    db.username           = ENV['MONGO_USER']
+    db.password           = ENV['MONGO_PASS']
+    db.host               = ENV['MONGO_HOST']
+    db.port               = ENV['MONGO_PORT']
+    db.ipv6               = false
+    db.only_collections   = [ENV['MONGO_COLLECTION']]
+    db.lock               = false
+    db.oplog              = false
+  end
+
+  store_with S3 do |s3|
+    s3.access_key_id = ENV['AWS_ACCESS_KEY_ID']
+    s3.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+    s3.region = ENV['AWS_REGION']
+    s3.bucket = ENV['AWS_BUCKET']
+    s3.path = ENV['AWS_MONGO_PATH']
+  end
+end
