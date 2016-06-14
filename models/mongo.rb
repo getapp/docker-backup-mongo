56217c26116d60b7535c86da088fbbf04e8e7955
getapp <tech+github@getapp.com>
2016-06-14 15:11:07 +0200

    add query support

--- models/mongo.rb
+++ models/mongo.rb
@@ -8,6 +8,9 @@ Model.new(:mongo, 'Mongo Backup') do
     db.ipv6               = false
     db.lock               = false
     db.oplog              = false
+    db.additional_options = [].tap |opts| do
+      opts << "--query=#{ENV['MONGO_QUERY']}" unless ENV['MONGO_QUERY']}.nil?
+    end
     unless ENV['MONGO_COLLECTIONS'].nil?
       db.only_collections = ENV['MONGO_COLLECTIONS'].split(',')
     end
